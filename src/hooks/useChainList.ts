d47277a80b06d32f7ceb262ab8a5a7f0defc691e
Zachary Belford <belfordz66@gmail.com>
2021-03-30 17:31:59 -0700

    fix: use a static short list for now

--- src/hooks/useChainList.ts
+++ src/hooks/useChainList.ts
@@ -6,21 +6,41 @@ import React from "react";
 const mergeChainSets = (c1: Chain[], c2: Chain[]) => uniqBy(c1.concat(c2), "name");
 
 export default function() {
-  const [chains, setChains] = React.useState<Chain[]>();
+  const [chains, setChains] = React.useState<Chain[]>([
+    {
+      name: "Matic",
+      network: "mainnet",
+      rpc: ["https://rpc-mainnet.matic.network"]
+    },
+    {
+      name: "Matic - Mumbai",
+      network: "testnet",
+      rpc: ["https://rpc-mumbai.matic.today"]
+    },
 
-  useEffect(() => {
-    if (chains === undefined) {
-      fetch("https://chainid.network/chains.json")
-        .then((r) => r.json())
-        .then((chainIdNetwork) => {
-          if (chains) {
-            setChains(mergeChainSets(chains, chainIdNetwork));
-          } else {
-            setChains(chainIdNetwork);
-          };
-        });
-    }
-  }, [chains]);
+  ]);
+
+  // uncomment once we add 'chain list provider' concept. This list blows.
+
+  // useEffect(() => {
+  //   if (chains === undefined) {
+  //     fetch("https://chainid.network/chains.json")
+  //       .then((r) => r.json())
+  //       .then((chainIdNetwork) => {
+  //         const filteredChains = chainIdNetwork.filter((c: Chain) => {
+  //           if (c.rpc.length === 0) {
+  //             return false;
+  //           }
+  //           return true;
+  //         });
+  //         if (chains) {
+  //           setChains(mergeChainSets(chains, filteredChains));
+  //         } else {
+  //           setChains(filteredChains);
+  //         };
+  //       });
+  //   }
+  // }, [chains]);
 
   return [chains, setChains];
 }
